c42abe2ee12211adf757d62259bdad1e69a1bdce
Masahiro Ito <itou@tdc-ict.co.jp>
2018-06-16 19:13:15 +0900

    App Center SDKの処理を追加

--- Phoneword/Phoneword/App.xaml.cs
+++ Phoneword/Phoneword/App.xaml.cs
@@ -1,6 +1,9 @@
 using System.Collections.Generic;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace Phoneword
@@ -19,6 +22,11 @@ namespace Phoneword
         protected override void OnStart()
         {
             // Handle when your app starts
+            AppCenter.Start("ios=00d2771e-1131-40c5-beee-9f5956f122c5;" +
+                "uwp={Your UWP App secret here};" +
+                "android={Your Android App secret here}",
+                typeof(Analytics), typeof(Crashes));
+
         }
 
         protected override void OnSleep()
